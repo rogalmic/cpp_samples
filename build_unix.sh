8e2c6891d5d371883afd77bf5979519afcd46d3b
ext-michal.rogalinski <michal.rogalinski@tieto.com>
2019-01-23 12:10:02 +0100

    cleanup of scripts, better to run directly in vscode tasks(no newline format problem)

--- build_unix.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_SYSTEM_VERSION=3.4.0 -DCMAKE_SYSTEM_NAME=Linux -Bunix -H. 
-cd unix && make
--- build_win.bat
+++
@@ -1,2 +0,0 @@
-cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_SYSTEM_VERSION=10.0.10586.0 -DCMAKE_SYSTEM_NAME=Windows -Bwin -H. 
-cd win && mingw32-make
