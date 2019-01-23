258d30bc17b49cffe7654cd0f1ccfff3a289e9f9
ext-michal.rogalinski <michal.rogalinski@tieto.com>
2019-01-23 10:46:17 +0100

    separate unix and windows builds

--- build_linux.sh
+++ build_linux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Debug -DCMAKE_SYSTEM_VERSION=3.4.0 -DCMAKE_SYSTEM_NAME=Linux . 
-make 
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_SYSTEM_VERSION=3.4.0 -DCMAKE_SYSTEM_NAME=Linux -Bunix -H. 
+cd unix && make
--- program.cpp
+++ program.cpp
@@ -5,5 +5,6 @@ using namespace std;
 
 int main(int argc, char *const argv[], char *const envp[])
 {
-    cout << "Hello world!";
+    cout << "Hello world!"; 
+    cout << "Hello world!2"; 
 }
