55e9168a235c007a76dcd8477eededa6657fddc4
ext-michal.rogalinski <michal.rogalinski@tieto.com>
2019-01-23 10:57:02 +0100

    renames win32->win, linux->unix, new project name

---
+++ build_unix.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_SYSTEM_VERSION=3.4.0 -DCMAKE_SYSTEM_NAME=Linux -Bunix -H. 
+cd unix && make
---
+++ build_win.bat
@@ -0,0 +1,2 @@
+cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_SYSTEM_VERSION=10.0.10586.0 -DCMAKE_SYSTEM_NAME=Windows -Bwin -H. 
+cd win && mingw32-make
