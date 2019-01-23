07c2bcb01573c342e6a24f3bf2869e5c9a43cba1
ext-michal.rogalinski <michal.rogalinski@tieto.com>
2019-01-23 14:42:30 +0100

    some sample code

--- program.cpp
+++ program.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 #include <fstream>
 #include <limits>
+#include <vector>
+#include <algorithm>
 using namespace std;
+
+void sort_test()
+{
+    vector<int> vect({445, 10, 15, 20, 20, 23, 42, 45}); 
+
+    std::cout << "Before: ";
+
+    for_each(vect.begin(), vect.end(), [](auto& v) {std::cout << v << ' ';});
+    
+    std::cout << endl;
+
+    sort(vect.begin(), vect.end()); 
+
+    std::cout << "After: ";
+
+    for_each(vect.begin(), vect.end(), [](auto& v) {std::cout << v << ' ';});
+
+    std::cout << endl;
+}
 
 int main(int argc, char *const argv[], char *const envp[])
 {
-    cout << "Hello world!"; 
-    cout << "Hello world!2"; 
+    sort_test();
 }
