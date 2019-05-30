c6e857c7dd4f338d6b6568803fafb24535fc497b
ext-michal.rogalinski <michal.rogalinski@tieto.com>
2019-05-30 15:49:39 +0200

    Add threading examples

---
+++ README.md
@@ -0,0 +1,11 @@
+# Mingw usage
+1. Install `MinGW-w64` and add `bin` folder to path
+1. Install `CMake` for windows
+1. Clear `win/` + `unix/` folders with cmake cache in case of probles.
+
+# WSL usage
+1. install WSL
+1. apt-get build essentials, cmake
+
+# Linux usage
+1. apt-get build essentials, cmake
--- program.cpp
+++ program.cpp
@@ -3,7 +3,17 @@
 #include <limits>
 #include <vector>
 #include <algorithm>
+#include <thread>
 using namespace std;
+
+void sort_test();
+void thread_test();
+
+int main(int argc, char *const argv[], char *const envp[])
+{
+    sort_test();
+    thread_test();
+}
 
 void sort_test()
 {
@@ -24,7 +34,23 @@ void sort_test()
     std::cout << endl;
 }
 
-int main(int argc, char *const argv[], char *const envp[])
+void thread_test()
 {
-    sort_test();
+    const int num_threads = 10;
+
+    std::thread t[num_threads];
+
+    for (int i = 0; i < num_threads; ++i)
+    {
+        t[i] = std::thread([](int tid) {
+            std::cout << "Launched by thread " << tid << std::endl;
+        }, i);
+    }
+
+    std::cout << "Launched" << std::endl;
+
+    for (int i = 0; i < num_threads; ++i)
+    {
+        t[i].join();
+    }
 }
